db0bf514e96e94453bc1ba637a234f60c422bb18
Paolo Montano <1@paolo.me>
2025-10-22 13:52:47 -0700

    Add config files for ghostty and fish.

---
+++ roles/macosx/files/fish/config.fish
@@ -0,0 +1,10 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Added by Toolbox App
+export PATH="$PATH:/Users/paolo/Library/Application Support/JetBrains/Toolbox/scripts"
+export XDG_CACHE_HOME="/Users/paolo/.cache"
+export XDG_CONFIG_HOME="/Users/paolo/.config"
+
+alias ll="ls -lah"
+alias mm="neomutt"
+alias opl="eval (op signin)"
