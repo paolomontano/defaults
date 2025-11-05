890f485d9abdb054478e08bd57fb2fa33e666129
Paolo Montano <1@paolo.me>
2025-11-04 23:52:03 -0800

    Added some CLI tool and aerospace.

--- roles/macosx/files/fish/config.fish
+++ roles/macosx/files/fish/config.fish
@@ -1,9 +1,15 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+starship init fish | source
+
 # Added by Toolbox App
 export PATH="$PATH:/Users/paolo/Library/Application Support/JetBrains/Toolbox/scripts"
 export XDG_CACHE_HOME="/Users/paolo/.cache"
 export XDG_CONFIG_HOME="/Users/paolo/.config"
+export STARSHIP_CONFIG="/Users/paolo/.config/starship/starship.toml"
+
+# Set up fzf key bindings
+fzf --fish | source
 
 alias ll="ls -lah"
 alias mm="neomutt"
